17b1368e4797a43676ca3543abb8cb1b0db2f5ec
hojin <hojin9car@gmail.com>
2023-10-04 14:28:07 +0900

    hojin 1

--- eng_bti/lib/main.dart
+++ eng_bti/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:flutter/services.dart';
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: QuestionPage(),
+      home: MainPage(),
     );
   }
 }
@@ -55,47 +56,135 @@ class _QuestionPageState extends State<QuestionPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xFFF2B3EE),
         title: Text("MBTI 조사"),
       ),
       body: Stack(
         children: [
           SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: questions.map((question) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: Text(question['text']),
-                      ),
-                      ...question['choices'].map<Widget>((choice) {
-                        return RadioListTile<String>(
-                          title: Text(choice['text']),
-                          value: choice['id'],
-                          groupValue: selectedAnswers[question['id']],
-                          onChanged: (value) {
-                            setState(() {
-                              selectedAnswers[question['id']] = value!;
-                            });
-                          },
-                        );
-                      }).toList(),
-                    ],
-                  );
-                }).toList(),
-              ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: questions.map((question) {
+                      return Card(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Padding(
+                              padding:
+                              const EdgeInsets.symmetric(vertical: 8.0),
+                              child: Text(
+                                ' Q. ' + question["text"],
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                            ...question['choices'].map<Widget>((choice) {
+                              return RadioListTile<String>(
+                                title:
+                                Text(choice['id'] + ': ' + choice['text']),
+                                value: choice['id'],
+                                groupValue: selectedAnswers[question['id']],
+                                onChanged: (value) {
+                                  setState(() {
+                                    selectedAnswers[question['id']] = value!;
+                                  });
+                                },
+                              );
+                            }).toList(),
+                          ],
+                        ),
+                      );
+                    }).toList(), // 여기에 버튼 추가.
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _checkEmptyAnswers,
+                  child: Text("제출하기"),
+                ),
+              ],
             ),
           ),
-          Positioned(
-            bottom: 10.0,
-            left: 10.0,
-            right: 10.0,
-            child: ElevatedButton(
-              onPressed: _checkEmptyAnswers,
-              child: Text("empty check"),
+        ],
+      ),
+    );
+  }
+}
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFFFFFFF),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/mainPageImg.jpg",
+              fit: BoxFit.contain,
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: Container(
+                  decoration: BoxDecoration(color: Colors.white54),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          BounceInUp(
+                            key: UniqueKey(),
+                            child: Text(
+                              '엥?',
+                              style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "BTI",
+                            style: TextStyle(
+                              fontSize: 40,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 40,
+                      ),
+                      // 나머지 위젯들
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFF2B3EE),
+                        ),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => QuestionPage(),
+                            ),
+                          );
+                        },
+                        child: Text('검사 시작'),
+                      )
+                    ],
+                  ),
+                ),
+              ),
             ),
           ),
         ],
